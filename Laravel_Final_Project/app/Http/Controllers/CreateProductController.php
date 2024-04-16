f5b3992af123fa3b10c4b61d8de75b090ff1736d
Zenlac <caleleizengrillera@gmail.com>
2024-04-17 02:08:01 +0800

    fixed Creation Function

--- Laravel_Final_Project/app/Http/Controllers/CreateProductController.php
+++ Laravel_Final_Project/app/Http/Controllers/CreateProductController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CreateProduct;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class CreateProductController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('CreateProduct.create');
+    }
+
+    public function store(Request $request)
+    {
+        $createproduct = new CreateProduct;
+        $createproduct->id = $request->input('id');
+        $createproduct->product = $request->input('product');
+        $createproduct->quantity = $request->input('quantity');
+        $createproduct->price = $request->input('price');
+        $createproduct->save();
+        return redirect()->back()->with('status','New Product has been Created');
+    }
 }
