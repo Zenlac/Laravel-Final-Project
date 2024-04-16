e0dcde5d72043ece1410c7a3db08935ecd86dff9
Zenlac <caleleizengrillera@gmail.com>
2024-04-16 21:21:30 +0800

    Added necessary files; controllers,routes,views etc.

--- Laravel_Final_Project/routes/web.php
+++ Laravel_Final_Project/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\InventoryController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Inventory', [InventoryController::class, 'Show']);
